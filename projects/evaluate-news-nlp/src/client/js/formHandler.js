8c87c8f03bdeb54cce2e57bd631bd6160ac070a6
Iten Elhak <56697800+Iten-No-404@users.noreply.github.com>
2022-02-26 00:10:01 +0200

    API Call Done

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -2,15 +2,50 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    let formText = document.getElementById('url').value
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    if(Client.checkForURL(formText)){
+        console.log("::: Form Submitted :::")
+        console.log({ url: formText })
+        fetch('http://localhost:8081/analyze', 
+        {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify({ url: formText })
+        })
+        .then(res => res.json())
+        .then(function(res) {
+            // document.getElementById('results').innerHTML = res.message
+            switch(res.score_tag){
+                case 'P+':
+                    document.getElementById('polarity').innerHTML = "Polarity = " + "Strong Positive";
+                    break;
+                case 'P':
+                    document.getElementById('polarity').innerHTML = "Polarity = " + "Positive";
+                    break;
+                case 'NEW':
+                    document.getElementById('polarity').innerHTML = "Polarity = " + "Neutral";
+                    break;
+                case 'N':
+                    document.getElementById('polarity').innerHTML = "Polarity = " + "Negative";
+                    break;
+                case 'N+':
+                    document.getElementById('polarity').innerHTML = "Polarity = " + "Strong Negative";
+                    break;
+                case 'NONE':
+                    document.getElementById('polarity').innerHTML = "Polarity = " + "No Sentiment";
+            }
+            document.getElementById('agreement').innerHTML = "Agreement = " + res.agreement;
+            document.getElementById('subjectivity').innerHTML = "Subjectivity = " + res.subjectivity;
+            document.getElementById('confidence').innerHTML = "Confidence = " + res.confidence;
+            document.getElementById('irony').innerHTML = "Irony = " + res.irony;
+        })
+    } else {
+        alert("Invalid URL! Please enter a valid article URL.")
+    }
 }
 
 export { handleSubmit }
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,20 +1,38 @@
 const dotenv = require('dotenv');
 dotenv.config();
-console.log(`Your API key is ${process.env.API_KEY}`);
-
 var path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
+const fetch = require('node-fetch');
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
+const cors = require('cors');
 
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 app.use(express.static('dist'))
+// app.use(express.json());
 
 console.log(__dirname)
+const meaningCloudURL = "https://api.meaningcloud.com/sentiment-2.1";
+console.log(`Your API key is ${process.env.API_KEY}`);
+
+let inputURL = ''
+let textapi = {
+    // application_id: process.env.API_ID,
+    key: process.env.API_KEY
+ };
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
     // res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
 })
 
 // designates what port the app will listen to for incoming requests
@@ -22,6 +40,12 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+app.post('/analyze', async function (req, res) {
+    inputURL = req.body.url;
+    console.log('Req.body=')
+    console.log(req.body.url)
+    const response = await fetch(meaningCloudURL+'?key='+process.env.API_KEY+'&url='+req.body.url+'&lang=en')
+    const data = await response.json()
+    console.log(data)
+    res.send(data)
 })
